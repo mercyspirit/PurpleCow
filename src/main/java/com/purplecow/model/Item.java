4ed0195042dd8417ad925a9c1002125af26e4c78
mercyspirit <ziying.qiu@gmail.com>
2022-03-12 09:30:01 -0500

    added the item model

---
+++ src/main/java/com/purplecow/model/Item.java
@@ -0,0 +1,28 @@
+package com.purplecow.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "items")
+public class Item {
+    @Id
+    private String id;
+    private String name;
+
+    public Item(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public String toString() {
+        return "Item: { id: " + id + ", name: " + name + " }";
+    }
+}
